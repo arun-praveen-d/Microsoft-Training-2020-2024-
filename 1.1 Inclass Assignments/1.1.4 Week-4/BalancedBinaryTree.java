e35e195290b855af287cf01e317e5bafe6bd99ff
arun-praveen-d <113841379+arun-praveen-d@users.noreply.github.com>
2022-09-25 20:03:08 +0530

    Update BalancedBinaryTree.java

--- 1.1 Inclass Assignments/1.1.4 Week-4/BalancedBinaryTree.java
+++ 1.1 Inclass Assignments/1.1.4 Week-4/BalancedBinaryTree.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)
+            return true;
+        
+        int lh = height(root.left);
+        int rh = height(root.right);
+        
+        if(Math.abs(lh-rh)>1)
+            return false;
+        
+        return isBalanced(root.left)&&isBalanced(root.right);
+    }
+    public int height(TreeNode root){
+        if(root==null)
+            return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+}
