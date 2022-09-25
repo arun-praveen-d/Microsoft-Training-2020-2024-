55bcfcd6131f5f0418864dba48ae28f2239a4448
arun-praveen-d <113841379+arun-praveen-d@users.noreply.github.com>
2022-09-25 20:22:55 +0530

    Update CircularQueue.java

--- 1.2 Assignment problems/1.2.3 Week 3/CircularQueue.java
+++ 1.2 Assignment problems/1.2.3 Week 3/CircularQueue.java
@@ -1,1 +1,48 @@
-//////////////////////////////
+class MyCircularQueue {
+       int[] circularArray;
+       int front=0;
+    int rear=-1;
+    int len=0; // to keep the count of number of elements
+    
+    public MyCircularQueue(int k) {
+      circularArray = new int[k];   
+    }
+    
+    public boolean enQueue(int value) {
+        if(!isFull()){
+             rear = (rear+1)%circularArray.length;
+            circularArray[rear] = value;
+            len++;
+            return true;
+        }
+        else
+        return false;
+    }
+    
+    public boolean deQueue() {
+        if(!isEmpty()){
+            front = (front+1)%circularArray.length;
+            len--;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public int Front() {
+        return isEmpty()?-1:circularArray[front];
+    }
+    
+    public int Rear() {
+        return isEmpty()?-1:circularArray[rear];
+    }
+    
+    public boolean isEmpty() {
+        return len == 0;
+    }
+    
+    public boolean isFull() {
+        return len==circularArray.length;
+    }
+}
+
