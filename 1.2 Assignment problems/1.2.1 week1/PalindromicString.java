a955fa2fd0e9bf9b2bb94ebfd7a7d4576c9964de
arun-praveen-d <113841379+arun-praveen-d@users.noreply.github.com>
2022-09-25 20:12:06 +0530

    Update PalindromicString.java

--- 1.2 Assignment problems/1.2.1 week1/PalindromicString.java
+++ 1.2 Assignment problems/1.2.1 week1/PalindromicString.java
@@ -1,1 +1,26 @@
-/////////////////////
+class Solution {
+    public int longestPalindrome(String s) {
+        if(s.length()==0)
+            return 0;
+        if(s.length()==1)
+            return 1;
+        
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            if(set.contains(s.charAt(i)))
+            {
+                set.remove(s.charAt(i));
+                count++;
+            }
+            else
+            {
+                set.add(s.charAt(i));
+            }
+        }
+        
+        if(!set.isEmpty())
+            return count*2+1;
+        else return count*2;
+    }
+}
