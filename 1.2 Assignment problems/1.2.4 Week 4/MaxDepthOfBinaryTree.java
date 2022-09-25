07e0b3c6feea7a0bac72b4f7e8cf070d8d6fcb77
arun-praveen-d <113841379+arun-praveen-d@users.noreply.github.com>
2022-09-25 20:28:17 +0530

    Update MaxDepthOfBinaryTree.java

--- 1.2 Assignment problems/1.2.4 Week 4/MaxDepthOfBinaryTree.java
+++ 1.2 Assignment problems/1.2.4 Week 4/MaxDepthOfBinaryTree.java
@@ -1,1 +1,13 @@
-///////////////
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        
+        return height(root);
+    }
+    public int height(TreeNode root){
+        if(root==null)
+            return 0;
+        
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+}
